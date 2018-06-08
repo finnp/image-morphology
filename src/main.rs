464ed00f6c03c031ff0efcd91ff9772726062c4a
Finn Pauls <derfinn@gmail.com>
2018-06-08 19:24:38 +0200

    basics working

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,16 @@ impl BinaryImage {
             height: height
         }
     }
+
+    fn get (&self, x: usize, y: usize ) -> bool {
+        if x >= self.width {
+            return false
+        }
+        if y >= self.height {
+            return false
+        }
+        self.data[x + y * self.width]
+    }
 }
 
 impl fmt::Display for BinaryImage {
@@ -29,7 +39,24 @@ impl fmt::Display for BinaryImage {
     }
 }
 
+fn dilate (input: BinaryImage) -> BinaryImage {
+    let mut output = BinaryImage::new(input.width, input.height);
+    for x in 1..input.width {
+        for y in 1..output.height {
+            let current = input.get(x, y);
+            let up = input.get(x, y - 1);
+            let down = input.get(x, y + 1);
+            let left = input.get(x - 1, y);
+            let right = input.get(x + 1, y);
+            output.data[x + y * output.width] = current || up || down || left || right;
+        }
+    }
+    output
+}
+
 fn main () {
-    let test = BinaryImage::new(10, 20);
-    println!("{}", test);
+    let mut test = BinaryImage::new(10, 20);
+    test.data[44] = true;
+    let output = dilate(test);
+    println!("{}", output);
 }
